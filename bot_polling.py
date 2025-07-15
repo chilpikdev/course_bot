23c161cab703eac43b2d869f8b5d9b2e5a1ebcee
Aybek <aybek.djumashev@gmail.com>
2025-07-15 10:22:33 +0500

    .

---
+++ bot_polling.py
@@ -0,0 +1,47 @@
+import requests
+import time
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'course_bot_project.settings') 
+django.setup()
+
+
+from bot.bot_manager import BotManager
+from bot.bot_handlers_simple import setup_bot_handlers
+
+BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+API_URL = f'https://api.telegram.org/bot{BOT_TOKEN}'
+OFFSET = None
+
+_bot_instance = None
+
+def get_bot_instance():
+    """Получить экземпляр бота (singleton)"""
+    global _bot_instance
+    if _bot_instance is None:
+        _bot_instance = BotManager(BOT_TOKEN)
+        setup_bot_handlers(_bot_instance)
+    return _bot_instance
+
+bot = get_bot_instance()   
+
+while True:
+    params = {'timeout': 10}
+    if OFFSET:
+        params['offset'] = OFFSET
+
+    response = requests.get(f'{API_URL}/getUpdates', params=params)
+    result = response.json()
+
+    if result.get("ok"):
+        updates = result.get("result", [])      
+        for update in updates:
+            bot.process_update(update)
+            OFFSET = update['update_id'] + 1                     
+            
+    else:
+        print(f"Error: {result.get('description')}")
+
+
+    time.sleep(.5)
